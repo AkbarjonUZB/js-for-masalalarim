62f77cd83ce99cf459acb0b3d247089f03fafdcc
Akbarjon <akbarjonraxmonov@1997gmail.com>
2024-01-22 23:21:55 +0500

    for

---
+++ main.js
@@ -0,0 +1,134 @@
+// for masalalari
+
+// for(i = 5; i<=17 ; i++){
+//    document.write(`i => ${i} <br>`)
+// }
+
+// masala - 2
+
+// for (let i = 9; i > -10 ; i--){
+//    document.write(` ${i} <br>`)
+// }
+
+// let n = +prompt("n sonini kirgazing "," ")
+
+// for(let i = 1; i <= n; i++ ){
+//      let x = n*i
+//    document.write(`${i} x ${x} <br>`)
+// }
+
+// // let kinfetNarhi = prompt("konfet narhini kirgazing ",' ')
+// let konfetNarhi = 30000
+// for(let i=1; i<=10; i++){
+//    let narxi = konfetNarhi * i;
+//    console.log(`${i} => ${narxi} kg`)
+// }
+
+
+
+// Uyga vazifa?
+// 1)
+
+// let n = 0;
+// for(let i=1; i<=100; i++ ){
+//    if(i % 5 ===0 && i % 3 === 0){
+
+//        n = n + i 
+//    }
+//    console.log(n)//// buni o'chirib qoysaham bolaveradi 
+// }
+// console.log(`yig'indi => ${n}`)
+
+// 2)
+
+// let a = +prompt("a sonini kirgazing "," ")
+// let b = +prompt("b sonini kirgazing "," ")
+
+// let x = 0;
+// for( let i=a; i<=b; i++){
+//   x=x+i;
+//   console.log(x) //// berkitb qoyish mumhun
+// }
+// console.log(`natija => ${x}`)
+
+// 1-masala)
+
+// let x = prompt("2 honalik raqam kirgaing ", " ")
+
+// let y = Math.floor(x/10);
+// let z = Math.floor(x%10);
+// console.log(y)
+// console.log(z)
+// console.log(y+z)
+
+
+// 3-masala)
+
+// let a = +prompt("a kesmani kirting "," ")
+// let b = +prompt("b kesmani kiriting "," ")
+
+//  let x = Math.floor(a/b)
+//  console.log(`a kesmaning ichda b kesma ${x} marta joylashadi`)
+
+// 4-masala 
+
+// let x = prompt("4 honalik raqam kirgaing ", " ")
+// let x=1234  //// Ishlatmaslik ham mumkiun
+// let Minglar = Math.floor(x/1000);
+// let Yuzlar = Math.floor(x/100 %10);
+// let Onlar = Math.floor(x/10%10);
+// let Birlar = Math.floor(x%10);
+   
+// console.log(`minglar ${Minglar}\n yuzlar ${Yuzlar}\n onlar ${Onlar}\n ONLAR ${Birlar}`)
+
+
+// for qishimch masalalar
+
+// for 1
+// let k = +prompt("k kesmani kirting "," ")
+// let n = +prompt("n kesmani kiriting "," ")
+
+// for(let i = 1; i<=n; i++){
+//    document.write(`${i} => ${k} <br>`)
+// }
+
+
+// for 2
+
+// let a = +prompt("a kesmani kirting "," ")
+// let b = +prompt("b kesmani kiriting "," ")
+// let s = 0
+// for(let i = a; i<=b; i++){
+//     console.log(i)
+//     s++   
+// }
+// console.log( ` raqamlar ${s} soni`)
+
+
+// for 3
+
+// let a = +prompt("a kesmani kirting "," ") 
+// let b = +prompt("b kesmani kiriting "," ") 
+// let s = 0;
+// for(let i = a; i>-b; i--){
+//     console.log(i)
+//     s++   
+// }
+// console.log( ` raqamlar ${s} soni`)
+
+
+// for 4 
+
+// let konfetNarhi = 12000;
+// for(let i=1; i<=10; i++){
+//    let x = konfetNarhi*i
+//    document.write(` ${i}  klagram konfet  narhi => ${x} <br>`)
+// }
+
+
+
+
+
+
+
+
